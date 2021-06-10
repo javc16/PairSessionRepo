2b05f20e31b6aa1081583dad2fd1c8e2e6b10d84
DESKTOP-AICR3S2\alexe <javc1603@gmail.com>
2021-06-10 14:22:52 -0600

    modifying NotImplementedException message

--- PairSessionRepo/TestingCode.cs
+++ PairSessionRepo/TestingCode.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PairSessionRepo
 {
@@ -11,9 +7,9 @@ namespace PairSessionRepo
         public decimal sum(decimal firstNumber, decimal secondNumber)
         {
             var result = firstNumber + secondNumber;
-            if (result > 10) 
+            if (result > 9) 
             {
-                throw new NotImplementedException("We only allow operations of one digit") ;
+                throw new NotImplementedException("We only allow operations of one digit result") ;
             }
 
             return result;
